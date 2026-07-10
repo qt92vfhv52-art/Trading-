2a7798c4f8e670789f77e839581dcdb8f515b5d5
qt92vfhv52-art <qt92vfhv52@privaterelay.appleid.com>
2026-07-10 18:42:55 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,67 @@
+"""
+data.py
+========
+تحميل بيانات الشموع من Binance باستخدام CCXT.
 
+Author: Mustafa Tariq
+"""
+
+import ccxt
+import pandas as pd
+
+
+exchange = ccxt.binance({
+    "enableRateLimit": True
+})
+
+
+def load_data(
+    symbol="BTC/USDT",
+    timeframe="1h",
+    limit=1000
+):
+    """
+    تحميل بيانات OHLCV من Binance.
+
+    Parameters
+    ----------
+    symbol : str
+        مثال: BTC/USDT
+
+    timeframe : str
+        مثال: 1m, 5m, 15m, 1h, 4h, 1d
+
+    limit : int
+        عدد الشموع
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
+
+    ohlcv = exchange.fetch_ohlcv(
+        symbol=symbol,
+        timeframe=timeframe,
+        limit=limit
+    )
+
+    df = pd.DataFrame(
+        ohlcv,
+        columns=[
+            "Date",
+            "Open",
+            "High",
+            "Low",
+            "Close",
+            "Volume"
+        ]
+    )
+
+    df["Date"] = pd.to_datetime(
+        df["Date"],
+        unit="ms"
+    )
+
+    df.set_index("Date", inplace=True)
+
+    return df

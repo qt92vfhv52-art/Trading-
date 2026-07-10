a1e156fabb557e2b3b02ef3ef88359a59ac4c7ab
qt92vfhv52-art <qt92vfhv52@privaterelay.appleid.com>
2026-07-10 20:34:19 +0300

    Update market_geometry.py

--- market_geometry.py
+++ market_geometry.py
@@ -1,1 +1,55 @@
+"""
+market_geometry.py
+==================
 
+Market Geometry Engine
+
+يقوم بتحويل بيانات OHLC
+إلى هيكل هندسي للسوق.
+
+Author: Mustafa Tariq
+"""
+
+import pandas as pd
+
+def get_swings(df):
+
+    highs = df[df["Swing High"]].copy()
+
+    lows = df[df["Swing Low"]].copy()
+
+    return highs, lows
+
+def build_pivots(df):
+    """
+    دمج القمم والقيعان
+    داخل DataFrame واحد.
+    """
+
+    pivots = []
+
+    for index, row in df.iterrows():
+
+        if row["Swing High"]:
+
+            pivots.append({
+                "Time": index,
+                "Type": "H",
+                "Price": row["High"]
+            })
+
+        if row["Swing Low"]:
+
+            pivots.append({
+                "Time": index,
+                "Type": "L",
+                "Price": row["Low"]
+            })
+
+    pivots = pd.DataFrame(pivots)
+
+    pivots.sort_values("Time", inplace=True)
+
+    pivots.reset_index(drop=True, inplace=True)
+
+    return pivots
